ebab62dce4e11f33149c2b27082706d98347dd63
AliCan1033 <turk.can.1071@gmail.com>
2021-07-17 17:20:56 +0300

    Adding Product

---
+++ 2ürün-ekleme/src/app/product-form/product-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../Model';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'product-form',
+  templateUrl: './product-form.component.html',
+  styleUrls: ['./product-form.component.css']
+})
+export class ProductFormComponent implements OnInit {
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+  }
+  addProduct(name:string,price:number,isActive:boolean){
+    console.log(name);
+    console.log(price);
+    console.log(isActive);
+
+    const p = new Product(this.productService.getProducts().length+1,name,price,isActive);
+    this.productService.addProduct(p);
+  }
+}
